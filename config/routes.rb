3decfb992624595ceba40c14cd67dabc2a54252e
Eder Martinez <edermtz.04@gmail.com>
2016-04-19 20:48:12 -0500

    change routes for comment's support

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 #set resources data
-	resources:articles
+	resources:articles do
+    resources :comments
+  end
 #set root
 	root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
